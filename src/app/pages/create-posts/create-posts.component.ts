fed64de73fe30d2175200e0031b4577a90ed5633
prathamVayana <pratham.mehta@vayana.com>
2025-05-13 15:03:25 +0530

    updated create post component to reduce un necessary piping

--- src/app/pages/create-posts/create-posts.component.ts
+++ src/app/pages/create-posts/create-posts.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { catchError, map, Observable, of } from 'rxjs';
 import { PostsService } from '../../services/posts.service';
 import { Title } from '@angular/platform-browser';
@@ -9,64 +14,51 @@ import { Post } from '../../models/posts';
   selector: 'app-create-posts',
   imports: [ReactiveFormsModule],
   templateUrl: './create-posts.component.html',
-  styleUrl: './create-posts.component.css'
+  styleUrl: './create-posts.component.css',
 })
 export class CreatePostsComponent {
-    private postsService = inject(PostsService);
-    postForm = new FormGroup({
-      title : new FormControl<string|null>(''),
-      post:new FormControl<string>(''),
-      tags: new FormControl<string[]>([])
-    })
+  private postsService = inject(PostsService);
+  postForm = new FormGroup({
+    title: new FormControl<string | null>(''),
+    post: new FormControl<string>(''),
+    tags: new FormControl<string[]>([]),
+  });
 
-    availableTags=["crime","suspense","mystery","comedy"]
+  availableTags = ['crime', 'suspense', 'mystery', 'comedy'];
 
-    onTagChange(tag: string, event:Event) {
-      const tagsControl = this.postForm.get('tags') as FormControl<string[]>;
-      const currentTags = tagsControl.value || [];
-      let isChecked:boolean = (event.target as HTMLInputElement).checked
-      if (isChecked) {
-        tagsControl.setValue([...currentTags, tag]);
-      } else {
-        tagsControl.setValue(currentTags.filter(t => t !== tag));
-      }
+  onTagChange(tag: string, event: Event) {
+    const tagsControl = this.postForm.get('tags') as FormControl<string[]>;
+    const currentTags = tagsControl.value || [];
+    let isChecked: boolean = (event.target as HTMLInputElement).checked;
+    if (isChecked) {
+      tagsControl.setValue([...currentTags, tag]);
+    } else {
+      tagsControl.setValue(currentTags.filter((t) => t !== tag));
     }
+  }
 
-    createPost(event:Event){
-      event.preventDefault();
+  createPost(event: Event) {
+    event.preventDefault();
 
-       const formValue = this.postForm.value;
+    const formValue = this.postForm.value;
 
     const post: Post = {
       id: Math.random(),
       title: formValue.title!,
       body: formValue.post!,
       tags: formValue.tags!,
-      userId:131,
+      userId: 131,
     };
 
-    const post$ = this.postsService.createPost(post).pipe(
-      catchError(err => {
-        console.error(err);
-        return of({
-          id: -1,
-          title: 'Error',
-          body: '',
-          tags: [],
-          userId:131
-        });
-      })
-    );
-
-    post$.subscribe({
-      next: result => console.log('Post created:', result),
-      error: err => console.error('Failed to create post:', err)
+    this.postsService.createPost(post).subscribe({
+      next: (result) => console.log('Post created:', result),
+      error: (err) => console.error('Failed to create post:', err),
     });
 
-      this.postForm.reset({
-        title:"",
-        post:"",
-        tags:[]
-      })
-    }
+    this.postForm.reset({
+      title: '',
+      post: '',
+      tags: [],
+    });
+  }
 }
